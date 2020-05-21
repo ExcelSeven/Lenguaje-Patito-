703c733ca6d7881b979c81bb71f003dded87a027
Martin Chung <martincc91@gmail.com>
2020-05-21 10:19:50 -0500

    Directorio de Funciones completo

---
+++ dirFunciones.py
@@ -0,0 +1,41 @@
+class Funciones:
+    def __init__(self, name, type, scope, vars):
+        self.name = name
+        self.type = type
+        self.scope = scope
+        self.vars = vars
+
+
+tablaVars = ["nomVar", "intV", "localV"]
+tablaVars2 = ["Variable2", "Float", "LOCAL"]
+
+func1 = Funciones("nombreF", "int", "global", tablaVars)
+func2 = Funciones("Funcion2", "float", "global", tablaVars2)
+
+print('Primer Print >> ', func1.name, func1.type, func1.scope, func1.vars)
+
+
+class DirFunciones:
+    def __init__(self):
+        self.directorio = dict()
+
+    def __set__(self, num, funcion):
+        self.directorio[num] = funcion
+        #print('__1set__ >> ', funcion)
+
+    def __getitem__(self, num):
+        return self.directorio[num]
+
+    def __contains__(self, num, funcion):
+        return num in self.directorio
+
+    def __repr__(self):
+        return self.directorio
+
+
+df = DirFunciones()
+df.__set__(0, func1)
+df.__set__(1, func2)
+
+print('Segundo Print >> ', vars(df.__getitem__(0)))
+print('Segundo Print >> ', vars(df.__getitem__(1)))

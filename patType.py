6be6d0a8814ea398548c8d8451400be87e61b8ff
Eli Moreira <coincidir777@hotmail.com>
2020-05-23 18:45:35 -0500

    Create patType.py

---
+++ patType.py
@@ -0,0 +1,27 @@
+class PatType:
+
+    def __init__(self):
+        self.type = ""
+        self.row = 0
+        self.col = 0
+
+    def type_key(self):
+        return self.type + "," + str(self.col) + "," + str(self.row)
+
+    def is_object(self):
+        return self.type not in ["Int", "Bool", "Float"]
+
+    def is_matrix(self):
+        return self.col > 0
+
+    def is_array(self):
+        return self.row > 0
+
+    def check_type(self, spark):
+        if self.type != spark.type:
+            return False
+        if self.row != spark.row:
+            return False
+        if self.col != spark.col:
+            return False
+        return True
